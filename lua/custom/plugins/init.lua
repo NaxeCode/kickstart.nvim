c268755968bbb395a18c4805ca0cbed2c2f648a0
Aladdin Ali <15963333+NaxeCode@users.noreply.github.com>
2024-10-16 14:14:25 -0400

    Updated my personal config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,31 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+return {
+  {
+    'wakatime/vim-wakatime',
+    lazy = false,
+  },
+  'jdonaldson/vaxe',
+
+  'rust-lang/rust.vim',
+
+  'wuelnerdotexe/vim-astro',
+
+  'loctvl842/monokai-pro.nvim',
+
+  'andweeb/presence.nvim',
+  {
+    'S1M0N38/love2d.nvim',
+    cmd = 'LoveRun',
+    opts = {},
+    keys = {
+      { '<leader>v', ft = 'lua', desc = 'LÖVE' },
+      { '<leader>vv', '<cmd>LoveRun<cr>', ft = 'lua', desc = 'Run LÖVE' },
+      { '<leader>vs', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
+    },
+  },
+}
